a78fa6960d913fea6f73a72d5c744eff1279609c
Rob Clayburn <rob@cognitivelogic.com>
2017-09-08 10:24:02 +0100

    add storybook webpack config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,21 @@
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const path = require('path');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  // Extend it as you need.
+
+  // For example, add typescript loader:
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: [
+      path.resolve(__dirname, '../src'),
+      path.resolve(__dirname, '../stories'),
+    ],
+    loader: require.resolve('ts-loader'),
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
